4f6a66a9d8cf1ea7dda9f0c791bab7f7480a807a
unknown <goodiswill2016@gmail.com>
2018-04-15 19:26:00 -0700

    04152018

---
+++ admin/application/language/english/extension/fee_lang.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Text
+$lang['text_title'] 	  	             = 'Fee';
+$lang['text_fee'] 	  	                 = 'Fee';
+$lang['text_extension'] 	  	         = 'Extension';
+$lang['text_no_fees'] 	  	             = 'No Fees';
+$lang['text_fee_description'] 	     	 = 'Display all shippings';
+$lang['text_install_success'] 	  	     = '<i class="fa fa-check-circle-o"></i>&nbsp;shipping install success';
+$lang['text_uninstall_success'] 	  	 = '<i class="fa fa-check-circle-o"></i>&nbsp;shipping uninstall success';
+
+// Column
+$lang['column_name'] 	  	             = 'Name';
+$lang['column_status'] 	  	             = 'Status';
+$lang['column_sort_order'] 	  	         = 'Sort Order';
+$lang['column_action'] 	  	             = 'Action';
