981a368954ae6972ce2ec4fe7db9dc7e3fb36fd4
que2018 <goodiswill2016@gmail.com>
2018-06-28 13:23:55 -0700

    06282018

---
+++ admin/application/libraries/Device.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once("device/Mobile_Detect.php");require_once("device/detect.php");	   	   
+class Device
+{
+	function __construct() 	{          }
+}
---
+++ admin/application/libraries/Sms.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once("Twilio/autoload.php");
+use Twilio\Rest\Client;
+class Sms
+{
+	public function send_sms($config, $phone, $content) 		
+	{
+		$client = new Client($config['sid'], $config['token']);
+		$client->messages->create(
+			$phone,
+			array(
+				'from' => $config['default_from'],
+				'body' => $content
+			)
+		);
+		return true;
+	}
+}
