1b71c0ccb95b89c89cba172352fe3e5456acb157
que2018 <goodiswill2016@gmail.com>
2019-06-20 15:40:48 -0700

    06202019

--- admin/application/views/sale/sale_list.php
+++ admin/application/views/sale/sale_list.php
@@ -66,7 +66,7 @@
 		    </div>
 		  </div>
 		  <div class="table-responsive">
-		    <table class="table table-striped table-bordered table-hover dataTables-example" >
+		    <table class="table table-striped table-bordered table-hover table-sale">
 			  <thead>
 			    <th style="width: 1px;" class="text-center"><input type="checkbox" onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" /></th>
 			    <?php if($sort == 'sale.id') { ?>
--- admin/application/views/sale/sale_list_table.php
+++ admin/application/views/sale/sale_list_table.php
@@ -44,7 +44,7 @@
   </div>
 </div>
 <div class="table-responsive">
-  <table class="table table-striped table-bordered table-hover dataTables-example" >
+  <table class="table table-striped table-bordered table-hover table-sale">
     <thead>
 	  <td style="width: 1px;" class="text-center"><input type="checkbox" onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" /></td>
 	  <?php if($sort == 'sale.id') { ?>
