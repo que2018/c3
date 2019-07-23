85048fc7f1a626640278a7f5ca11e4aa29212ebb
que2018 <goodiswill2016@gmail.com>
2019-07-22 22:41:00 -0700

    07222019

--- admin/application/models/shipping/Sf_model.php
+++ admin/application/models/shipping/Sf_model.php
@@ -8,8 +8,46 @@ class Sf_model extends CI_Model
 	{
 		$this->db->delete('setting', array('code' => 'sf')); 
 	}
-}
+	
+	public function generate_sale_label($sale_id)
+	{
+		$ch = curl_init();
 
+		$data['Order'] = array(
+			'OrderId'           => 'value1',
+			'CneeContactName'   => 'value1',
+			'CneeCompany'       => 'value1',
+			'CneeAddress'       => 'value1',
+			'CneeCity'          => 'value1',
+			'CneeProvince'      => 'value1',
+			'CneeCountry'       => 'value1',
+			'CneePostCode'      => 'value1',
+			'CneePhone'         => 'value1',
+			'CneeEmail'         => 'value1',
+			'ReferenceNo1'      => 'value1',
+			'ExpressType'       => 'value1',
+			'ParcelQuantity'    => 'value1',
+			'PayMethod'         => 'value1',
+			'TaxPayType'        => 'value1',
+			'Currency'          => 'value1',
+			'Items'             => 'value1',
+			'GateWay'           => 'value1',
+			'postvar1'          => 'value1',
+			'postvar1'          => 'value1'
+		);
+
+		curl_setopt($ch, CURLOPT_URL,"https://sit-api.sf-express-us.com/api/orderservice/submitorder");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$output = curl_exec($ch);
+
+		curl_close ($ch);
+		
+		file_put_contents("log.txt", serialize($output));
+	}
+}
 
 
 
