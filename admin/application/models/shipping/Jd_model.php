334b063bf6de208b346614d93924945e5589b70e
que2018 <goodiswill2016@gmail.com>
2019-08-09 14:56:23 -0700

    08092019

--- admin/application/models/shipping/Jd_model.php
+++ admin/application/models/shipping/Jd_model.php
@@ -18,8 +18,6 @@ class Jd_model extends CI_Model
 		$sale_detail = $this->sale_model->get_sale_detail($sale_id);
 		$sale_products = $this->sale_model->get_sale_products($sale_id);
 
-		$volumn = $sale['length'] * $sale['width'] * $sale['height'];
-
 		$goodsDtoList = array();
 		
 		foreach($sale_products as $sale_product)
@@ -31,38 +29,45 @@ class Jd_model extends CI_Model
 				'productCount'         => $sale_product['quantity'],
 				'halfReason'           => 0,
 				'productNameLocleList' => ''
-				
 			);
 		}
 		
 		$data = array(
-			'orderId'           => $sale['id'],
 			'source'            => $this->config->item('jd_source'),
-			'packageCount'      => 1,
-			'senderAddress'     => $this->config->item('jd_sender_address'),
+			'salePlat'          => $this->config->item('jd_sale_plat'),
+			'customerCode'      => $this->config->item('jd_customer_code'),
+			'customerName'      => $sale['name'],
+			'orderId'           => $sale['id'],
+			'thrOrderId'        => $sale['id'],
 			'senderName'        => $this->config->item('jd_sender_name'),
+			'senderAddress'     => $this->config->item('jd_sender_address'),
+			'senderMobile'      => $this->config->item('jd_sender_mobile'),
 			'senderProvince'    => $this->config->item('jd_sender_province'),
 			'senderCity'        => $this->config->item('jd_sender_city'),
 			'senderPostcode'    => $this->config->item('jd_sender_postcode'),
 			'receiveName'       => $sale['name'],
 			'receiveAddress'    => $sale['street'],
-			'Province'          => $sale['state'],
+			'receiveTel'    	=> $sale['phone'],
+			'receiveMobile'    	=> $sale['phone'],
+			'province'          => $sale['state'],
 			'city'              => $sale['city'],
-			'country'           => $sale['country'],
 			'postcode'          => $sale['zipcode'],	
-			'salePlat'          => $this->config->item('jd_sale_plat'),
-			'serviceLevel'      => $this->config->item('jd_service_level'),
-			'pickMethod'        => $this->config->item('jd_service_pickup_method'),
-			'description'       => $sale_detail,
+			'packageCount'      => 1,
 			'collectionValue'   => $this->config->item('jd_collection_value'),
-			'goodsType'         => $this->config->item('jd_good_type'),
+			'country'           => $sale['country'],
+			'urlArgs'           => $this->config->item('jd_url_args'),
+			'serviceLevel'      => $sale['shipping_service'],	
 			'goodsDtoList'      => $goodsDtoList
+			//'collectionMoney'   => 100.0,
+			//'pickMethod'        => $this->config->item('jd_service_pickup_method'),
+			//'description'       => $sale_detail,
+			//'collectionValue'   => 
+			//'goodsType'         => $this->config->item('jd_good_type'),
 		);
-					
-		//$payload = "[".json_encode($data)."]";	
-        $payload = "[{'source':'ISV','salePlat':'0010001','customerCode':'010K101','customerName':'ec139','orderId':'676555512','thrOrderId':'676555512','senderName':'MrGao','senderAddress':'Pergudangan Marunda Center, Sagara Makmur','senderMobile':'18811700000','senderProvince':'California','senderCity':'Fullerton','senderPostcode':'92831','receiveName':'GaoYaofei','receiveAddress':'Beijingdaxin','receiveTel':'1321313213','receiveMobile':'18811766317','province':'Arizona','city':'Casa Grande','postcode':'85193','packageCount':1,'collectionValue':1,'collectionMoney':100.00,'country':'US','urlArgs':'lyz'}]";
 				
-		$post_url = "http://test-gateway.jdwl.com/b2b/qli18n_waybill/glscOuterMerchantOrder/i18nReceiveOrderIsv";
+		$payload = "[".json_encode($data)."]";	
+								
+		$post_url = "http://us-api.jd.com/b2b/ql_i18n_ldop_us/outerMerchantOrder/glscReceiveOrderUat";
 
 		$content_md5 = MD5($payload);
 		
@@ -91,52 +96,124 @@ class Jd_model extends CI_Model
 
 		$output = curl_exec($ch);
 		
-		curl_close ($ch);
-
+		curl_close($ch);
+		
+		$response = json_decode($output);
+		
 		/* ------------------------ login begin ------------------------ */
 		
-		file_put_contents("log.txt", "");
-		file_put_contents("log.txt", "-------- xdate -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $xdate . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- content md5 -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $content_md5 . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- txt -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $txt . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- macUser -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $macUser . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- macKey -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $macKey . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- sign -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $sign . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- auth -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $auth . "\n\n", FILE_APPEND);
-		file_put_contents("log.txt", "-------- response -------- \n\n", FILE_APPEND);
-		file_put_contents("log.txt", $output . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "");
+		file_put_contents("order.txt", "-------- xdate -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $xdate . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- content md5 -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $content_md5 . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- txt -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $txt . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- macUser -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $macUser . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- macKey -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $macKey . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- sign -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $sign . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- auth -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $auth . "\n\n", FILE_APPEND);
+		file_put_contents("order.txt", "-------- response -------- \n\n", FILE_APPEND);
+		file_put_contents("order.txt", $output . "\n\n", FILE_APPEND);
 
 		/* ------------------------ login end ------------------------ */
+		
+		
+		if($response->resultCode == 100)
+		{	
+			sleep(10);
+	
+			$delivery_id = $response->deliveryId;
+									
+			$result = $this->print_label($sale_id, $delivery_id);
+		} 
+		else
+		{
+			$result['error'] = $response->statusMessage;
+		}
+		
+		return $result;
 	}
 	
-	private function generate_jd_label($sale_id, $deliveryId)
+	public function print_label($sale_id, $delivery_id)
 	{
 		$data = array(
-			'customerId'  => '',
+			'customerId'  => 41,
 			'orderId'     => $sale_id,
-			'deliveryId'  => $deliveryId,
+			'deliveryId'  => $delivery_id,
 			'urlArgs'     => $this->config->item('jd_url_args')
 		);
-						
+		
+		$payload = "[".json_encode($data)."]";	
+		
+		file_put_contents("payload.txt", $payload);
+		
+		$post_url = "http://us-api.jd.com/b2b/ql_i18n_ldop_us/outerMerchantPrint/glscPrint4Uat";
+					
+		$content_md5 = MD5($payload);
+		
+		$macUser = $this->config->item('jd_user');
+		$macKey = $this->config->item('jd_secret_key');
+
+		$xdate = $this->get_server_time();
+		$txt = "X-Date: " . $xdate . "\n" . "content-md5: " . $content_md5;
+		$sign = $this->HmacSHA1Encrypt($txt, $macKey);		
+		$auth = "hmac username=\"" . $macUser . "\", algorithm=\"hmac-sha1\", headers=\"X-Date content-md5\",signature=\"" . $sign . "\"";
+		
 		$ch = curl_init();	
 
-		curl_setopt($ch, CURLOPT_URL,"https://sit-api.sf-express-us.com/api/orderservice/submitorder");
+		curl_setopt($ch, CURLOPT_URL, $post_url);
+		
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'X-Date: '.$xdate,
+			'content-md5: '.$content_md5,
+			'Authorization: '.$auth,
+			'content-type: application/json'
+		));
+
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 		$output = curl_exec($ch);
 		
-		file_put_contents("label.txt", serialize($output));
-
-		curl_close ($ch); 
+		curl_close($ch);
+				
+		$response = json_decode($output);
+		
+		file_put_contents("label.txt", $output);
+		
+		if($response->statusCode == 0)
+		{
+			$data = $response->data;
+						
+			foreach($data as $tracking => $base64_strings) 
+			{
+				$result['tracking'] = $tracking;
+				$result['amount'] = 30;
+				$result['label_img'] = '';
+				
+				foreach($base64_strings as $index => $base64_string)
+				{	
+					$pdf_decoded = base64_decode($base64_string);
+					$pdf = fopen($index.'.pdf','w');
+					fwrite($pdf, $pdf_decoded);
+					fclose($pdf);
+				
+					//$this->base64_to_pdf($value);
+				}
+			}
+		}
+		else
+		{
+			$result['error'] = $response->statusMessage;
+		}
+		
+		return $result;
 	}
 	
 	private function get_server_time() {		
@@ -148,17 +225,19 @@ class Jd_model extends CI_Model
 	private function HmacSHA1Encrypt(String $encryptText, String $encryptKey )
 	{
 		$signature = "";
-		if(function_exists('hash_hmac')){
-	 
+		
+		if(function_exists('hash_hmac'))
+		{
 			$hash_hmac = hash_hmac("sha1",$encryptText,$encryptKey,true);
 			$signature = base64_encode($hash_hmac);
 	 
-		}else{
-	 
+		}else
+		{
 			$blocksize = 64;
 			$hashfunc  = 'sha1';
 	 
-			if(strlen($encryptKey) > $blocksize) {
+			if(strlen($encryptKey) > $blocksize) 
+			{
 				$encryptKey = pack("H*", $hashfunc($encryptKey));
 			}
 	 
@@ -168,7 +247,15 @@ class Jd_model extends CI_Model
 			$hmac = pack("H*", $hashfunc(($encryptKey ^ $opad).pack('H*', $hashfunc(($encryptKey ^ $ipad).$str))));
 			$signature = base64_encode($hmac);
 		}
+		
 		return $signature;
+	}
+	
+	private function base64_to_pdf($base64_string) {
+		$pdf_decoded = base64_decode($base64_string);
+		$pdf = fopen ('test.pdf','w');
+		fwrite($pdf,$pdf_decoded);
+		fclose($pdf);
 	}
 }
 
